ffbc99be7ff684c4b531986617cf18eecc359c84
tucecifci <tucecifcii@gmail.com>
2024-11-29 13:03:05 +0300

    clear list feature completed

--- src/App.jsx
+++ src/App.jsx
@@ -28,12 +28,18 @@ function handleToggleItem(id){
 setItems((items) => items.map((item) => item.id === id ? {...item, packed: !item.packed} : item));
 }
 
+function handleClearList(){
+const confirmed = window.confirm("Are you sure you want to delete all items?");
+
+  if(confirmed) setItems([]);
+}
+
   return (
     <>
     <div className='app'>
      <Logo />
      <Form onAddItems= {handleAddItems} />
-     <PackingList items={items} onDeleteItem={handleDeleteıtem} onToggleItem={handleToggleItem} />
+     <PackingList items={items} onDeleteItem={handleDeleteıtem} onToggleItem={handleToggleItem} onClearList={handleClearList} />
      <Stats items={items} />
     
      </div>
